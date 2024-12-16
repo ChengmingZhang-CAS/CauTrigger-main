6e3ccfe76253ef821559e1dc332abb169e16d141
alahenell <alahenell@163.com>
2024-12-16 17:28:05 +0800

    change tutorials

---
+++ tutorials/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CauTrigger'
+copyright = '2024, Chengming Zhang'
+author = 'Chengming Zhang'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'nbsphinx',  # Jupyter Notebook support
+    'myst_parser'  # Markdown support
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Explicitly set file types
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
